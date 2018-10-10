3236688db5230ef7af800fe1bf3f21f654b5db1e
Nazar Redko <rednaz1990@gmail.com>
2018-10-10 11:04:18 +0300

    test

--- src/simple_api/settings.py
+++ src/simple_api/settings.py
@@ -170,7 +170,7 @@ class Dev(Base):
 
     CLEARBIT_KEY = values.Value('xxx')
     HUNTER_API_KEY = values.Value('xxx')
-    ALLOWED_HOSTS = ['.herokuapp.com', 'http://django-env.d2jbdqvssc.eu-central-1.elasticbeanstalk.com']
+    ALLOWED_HOSTS = ['localhost', '.herokuapp.com', 'http://django-env.d2jbdqvssc.eu-central-1.elasticbeanstalk.com']
 
 
 class Prod(Base):
@@ -184,7 +184,7 @@ class Prod(Base):
     CLEARBIT_KEY = values.Value('xxx')
     HUNTER_API_KEY = values.Value('xxx')
 
-    ALLOWED_HOSTS = ['.herokuapp.com', 'http://django-env.d2jbdqvssc.eu-central-1.elasticbeanstalk.com']
+    ALLOWED_HOSTS = ['localhost', '.herokuapp.com', 'http://django-env.d2jbdqvssc.eu-central-1.elasticbeanstalk.com']
     STATIC_ROOT = os.path.join(Base.BASE_DIR, 'staticfiles')
     DATABASES = Base.DATABASES.copy()
     DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
